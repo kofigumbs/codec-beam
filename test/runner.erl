c7287158ef6ca01793c7a2fbbb657814a23aeaa6
Kofi Gumbs <h.kofigumbs@gmail.com>
2017-04-30 12:59:07 -0500

    Catch internal errors in runner

--- test/runner.erl
+++ test/runner.erl
@@ -5,11 +5,21 @@ main([BeamFile, ChunkName]) ->
     { ok, Bin } = file:read_file(BeamFile),
     Id = list_to_atom(ChunkName),
 
-    case beam_lib:chunks(Bin, [Id]) of
-        { ok, Chunks } -> io:fwrite("~w", [Chunks]);
-        { error, _, Reason } -> io:fwrite("~w", [Reason])
+    case catch beam_lib:chunks(Bin, [Id]) of
+        { ok, Chunks } ->
+            io:fwrite("~w", [Chunks]);
+
+        { error, _, Reason } ->
+            fail(beam_lib:format_error(Reason));
+
+        { 'EXIT', Reason } ->
+            fail(io_lib:format("~p", [Reason]))
     end;
 
 main(_) ->
-    io:fwrite("Usage : escript runner BEAM_FILE CHUNK_NAME"),
+    fail("Usage : escript runner BEAM_FILE CHUNK_NAME").
+
+
+fail(Message) ->
+    io:put_chars(standard_error, Message),
     halt(1).
