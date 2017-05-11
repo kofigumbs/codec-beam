f89449a9ae694e1f7b9119b202b32b76bb31b08e
Kofi Gumbs <h.kofigumbs@gmail.com>
2017-05-10 21:57:15 -0500

    Clean up test DSL

--- test/Main.hs
+++ test/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
-import qualified Data.ByteString.Lazy as BS
-import Data.Text.Lazy (unpack)
-import Data.Text.Lazy.Encoding (decodeUtf8)
-import System.Process (callProcess)
+import Data.Monoid ((<>))
+import Data.Text.Lazy (pack, unpack)
+import Data.Text.Lazy.Encoding (encodeUtf8, decodeUtf8)
 import System.FilePath ((</>), (<.>))
+import System.Process (callProcess)
+import qualified Data.ByteString.Lazy as BS
+
+import Prelude hiding (unlines)
 
 import qualified Codec.Beam as Beam
 
@@ -19,36 +22,52 @@ erlangModuleName =
   "codec_tests"
 
 
-eunit :: (BS.ByteString, String, Beam.Builder [Beam.Instruction]) -> IO String
-eunit (name, body, beam) =
-  do  let stringName =
-            unpack (decodeUtf8 name)
+unlines :: [BS.ByteString] -> BS.ByteString
+unlines =
+  BS.intercalate "\n"
 
-          fixture =
-            erlangDir </> stringName <.> "beam"
+
+toString :: BS.ByteString -> String
+toString =
+  unpack . decodeUtf8
+
+
+fromString :: String -> BS.ByteString
+fromString =
+  encodeUtf8 . pack
+
+
+test
+  :: BS.ByteString
+  -> [BS.ByteString]
+  -> Beam.Builder [Beam.Instruction]
+  -> IO BS.ByteString
+test name body beam =
+  do  let fixture =
+            erlangDir </> toString name <.> "beam"
 
       BS.writeFile fixture (Beam.encode name [] beam)
 
       return $
         unlines
-          [ stringName ++ "_test() ->"
-          , "{ok,BEAM} = file:read_file(\"" ++ fixture ++ "\"),"
-          , body ++ "."
+          [ name <> "_test() ->"
+          , "{ok,BEAM} = file:read_file(\"" <> fromString fixture <> "\"),"
+          , unlines body <> "."
           ]
 
 
-run :: [String] -> IO ()
+run :: [BS.ByteString] -> IO ()
 run functions =
   do  let fileContents =
             unlines $
-              ("-module(" ++ erlangModuleName ++ ").")
+              "-module(" <> fromString erlangModuleName <> ")."
                 : "-include_lib(\"eunit/include/eunit.hrl\")."
                 : functions
 
           fileName =
             erlangDir </> erlangModuleName <.> "erl"
 
-      writeFile fileName fileContents
+      BS.writeFile fileName fileContents
 
       callProcess "erlc" [fileName]
 
@@ -61,30 +80,28 @@ run functions =
 
 main :: IO ()
 main =
-  run =<< mapM eunit
-    [ ( "empty"
-      , "?assertMatch(\
-          \ {ok, {empty, [{imports, []},{labeled_exports, []},{labeled_locals, []}]}},\
-          \ beam_lib:chunks(BEAM, [imports, labeled_exports, labeled_locals]))"
-      , return []
-      )
+  run =<< sequence
+    [ test "empty"
+        [ "?assertMatch("
+        , "  {ok, {empty, [{imports, []},{labeled_exports, []},{labeled_locals, []}]}},"
+        , "  beam_lib:chunks(BEAM, [imports, labeled_exports, labeled_locals]))"
+        ] $
+        return []
 
-    , ( "module_atom"
-      , "?assertMatch(\
-          \ {ok, {module_atom, [{atoms, [{1,module_atom}]}]}},\
-          \ beam_lib:chunks(BEAM, [atoms]))"
-      , return []
-      )
+    , test "module_atom"
+        [ "?assertMatch("
+        , "  {ok, {module_atom, [{atoms, [{1,module_atom}]}]}},"
+        , "  beam_lib:chunks(BEAM, [atoms]))"
+        ] $
+        return []
 
-    , ( "constant_function"
-      , unlines
-          [ "{module, constant_function} = code:load_binary(constant_function, BEAM),"
-          , "?assertEqual(hello, constant_function:function())"
-          ]
-      , do  function <- Beam.funcInfo "constant_function" 0
+    , test "constant_function"
+        [ "{module, constant_function} = code:load_binary(constant_function, BEAM),"
+        , "?assertEqual(hello, constant_function:function())"
+        ] $
+        do  function <- Beam.funcInfo "constant_function" 0
             hello <- Beam.atom "hello"
             (_, label) <- Beam.label
 
             return [ function , label , Beam.move (Beam.Atom hello) (Beam.X 0) ]
-      )
     ]
