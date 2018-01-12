80cdfb6d252a1e941b0f651ef51e3af7f3b420b3
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-01-11 20:28:25 -0600

    Re-work types to capture all tab lines

--- gen/Ops.hs
+++ gen/Ops.hs
@@ -1,70 +1,113 @@
-module Ops (Op(..), Arg(..), Ops.parse) where
+module Ops (Line(..), Type(..), Ops.parse) where
 
 import Data.Either (rights)
-import Text.Parsec
+import Text.Parsec hiding (Line)
 import Text.Parsec.String (Parser)
+import qualified Data.Set as Set
 
 
-data Op =
-  Op
+data Line
+  = GenericOp Int
+  | SpecificOp String [Type]
+  | Match [Pattern] Body
+
+
+data Body
+  = C
+  | Ops [Pattern]
+
+
+data Pattern =
+  Pattern
     { _name :: String
-    , _args :: [Arg]
+    , _args :: [Argument]
     }
-  deriving Show
 
 
-data Arg
+data Argument
+  = Named String Type
+  | Constrained Type
+  | Anonymous String
+
+
+data Type
   = XRegister
   | YRegister
   | FloatRegister
   | IntTagged
   | IntUntagged
+  | WordUntagged
   | Atom
   | Nil
   | Literal
   | Label
   -- TODO: include loader-only types?
-  deriving Show
+  deriving (Eq, Ord, Show)
 
 
-parse :: String -> [Op]
-parse input =
-  concat $ rights $ map (Text.Parsec.parse genop "ops.tab") (lines input)
+parse :: String -> Either ParseError [Line]
+parse =
+  Text.Parsec.parse (sepBy line newline) "ops.tab"
 
 
-genop :: Parser [Op]
-genop =
-  do  ops <- sepBy1 declaration (string " | ")
-      choice [ eof, ignore string " =>" ]
-      return ops
+line :: Parser Line
+line =
+  skipLine '#' <|> skipLine '%' <|> genericOp <|> specificOp <|> pattern
 
 
-declaration :: Parser Op
+skipLine :: Char -> Parser Line
+skipLine c =
+  char c >> manyTill anyChar newline >> line
+
+
+genericOp :: Parser Line
+genericOp =
+  error "TODO"
+
+
+specificOp :: Parser Line
+specificOp =
+  error "TODO"
+
+
+pattern :: Parser Line
+pattern =
+  error "TODO"
+
+
+declaration :: Parser (String, [Set.Set Type])
 declaration =
   do  name <- many1 (lower <|> digit <|> char '_')
       args <- many argument
-      return $ Op name args
+      pure $ (name, args)
 
 
-argument :: Parser Arg
+argument :: Parser (Set.Set Type)
 argument =
-  do  space
-      optional variable
-      type_ <- choice
-        [ tag 'x' XRegister
-        , tag 'y' YRegister
-        , tag 'l' FloatRegister
-        , tag 'i' IntTagged
-        , tag 'q' IntUntagged
-        , tag 'a' Atom
-        , tag 'n' Nil
-        , tag 'q' Literal
-        , tag 'f' Label
-        , tag 'p' Label
-        ]
+  do  firstType <- try (space >> optional variable >> type_)
+      otherTypes <- many type_
       optional constraint <|> ignore char '?'
-      return type_
+      pure $ Set.fromList (firstType : otherTypes)
 
+
+type_ :: Parser Type
+type_ =
+  choice
+    [ char 'x' >> pure XRegister
+    , char 'y' >> pure YRegister
+    , char 'l' >> pure FloatRegister
+    , char 'i' >> pure IntTagged
+    , char 'q' >> pure IntUntagged
+    , char 'u' >> pure WordUntagged
+    , char 'L' >> pure WordUntagged
+    , char 'a' >> pure Atom
+    , char 'n' >> pure Nil
+    , char 'q' >> pure Literal
+    , char 'c' >> pure Literal
+    , char 'f' >> pure Label
+    , char 'p' >> pure Label
+    , string "Lbl" >> pure Label
+    ]
 
 variable :: Parser ()
 variable =
@@ -74,15 +117,10 @@ variable =
 
 constraint :: Parser ()
 constraint =
-  do  choice [ ignore char '$', ignore string "==" ]
+  do  ignore char '$'<|> try (ignore string "==")
       ignore many1 $ satisfy (/= ' ')
-
-
-tag :: Char -> Arg -> Parser Arg
-tag character value =
-  char character >> return value
 
 
 ignore :: (a -> Parser b) -> a -> Parser ()
 ignore toParser a =
-  toParser a >> return ()
+  toParser a >> pure ()
