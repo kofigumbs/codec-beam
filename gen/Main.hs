92b89311dee9c6d5cf803602a97293df53633cc0
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-01-13 07:58:02 -0600

    Pass version via command-line

--- gen/Main.hs
+++ gen/Main.hs
@@ -1,7 +1,13 @@
-import qualified Ops
-import System.Process
+import System.Process (readProcess)
+import System.Environment (getArgs)
 
+import qualified Ops
 
 main :: IO ()
 main =
-  print =<< Ops.parse <$> readProcess "curl" [ Ops.downloadUrl "maint-20" ] ""
+  do  commandLineArguments <- getArgs
+      case commandLineArguments of
+        [] ->
+          error "Missing erlang version command-line argument!"
+        v : _ ->
+          print =<< Ops.parse <$> readProcess "curl" [ Ops.downloadUrl v ] ""
