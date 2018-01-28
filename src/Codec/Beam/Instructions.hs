2e2017c41b13770f2df46573f4d567b8798d5012
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-01-27 20:19:59 -0600

    No more need for ranked types

--- src/Codec/Beam/Instructions.hs
+++ src/Codec/Beam/Instructions.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE Rank2Types #-}
-
 -- | This module represents a type-safe port of Erlang's general instructions.
 --   In the end state, there should only be few variations,
 --   existing only to promote ease of use and correctness!
